a5b7981aae5d4a43985e6d765f54ab6fbb490293
Дарья Князева <knyazeva.darya1234@gmail.com>
2021-11-13 18:27:22 +0300

    re1

--- release-1/index.js
+++ release-1/index.js
@@ -1,30 +1,54 @@
 //  'Nikita' -> 'Hi, Nikita'
- const hiFunction = () => {
-    //code here
-}
+hiFunction()
+ function hiFunction() {
+     let name = "Nikita"
+     console.log(name)
+ } 
+
 
 
 // 5, 6 -> 11
- const sum = () => {
+sum()
+ function sum() {
     //code here
+    let x = 5
+    let y = 6
+    console.log(x+y)
 }
 
 
 //5 -> false
 //0 -> true
 //4 -> true
- const isEven = () => {
+isEven()
+ function isEven() {
     //code here
+    var even = 5
+    if (even % 2 == 0) {
+        console.log(true)
+    }
+    else {
+        console.log(false)
+    }
 }
 
 
 // 2 -> i have 2 apples
- const apples = () => {
+apples()
+ function apples() {
     //code here
+    let number = 2
+    let phrase = "i have 2 apples"
+    if (2) {
+        console.log(phrase)
+    }
 }
 // 4 -> 16
- const getPower = () => {
+getPower()
+ function getPower () {
     //code here
+    let num = 99
+    console.log(num*num)
 }
 
 module.exports = {
